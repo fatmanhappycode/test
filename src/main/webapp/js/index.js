359a2397a44ad8613ef80a8f25a881dcd50e42b6
fatmanhappycode <1943279276@qq.com>
2019-09-19 19:50:26 +0800

    页面

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,21 +1,17 @@
 $(document).on("click","#login",function () {
-    var username = $("#username").val();
-    var password = $("#password").val();
+    var username = $("#inputEmail").val();
+    var password = $("#inputPassword").val();
     $.ajax({
         url:"/login",
         type:"POST",
         data:{username:username, password:password},
         success:function (result) {
             if (result.code == 200) {
-                alert("登录成功");
-                to_Page(totalRecord);
+                alert(result.msg);
+            } else if (result.code == 401) {
+                alert(result.msg);
             } else {
-                if (undefined != result.extend.errorFields.email) {
-                    show_validate_msg("#email_add_input", "error", result.extend.errorFields.email);
-                }
-                if (undefined != result.extend.errorFields.empName) {
-                    show_validate_msg("#email_add_input", "error", result.extend.errorFields.empName);
-                }
+                // doSomething
             }
         }
     })
