fd2cce79436278b87a4d995ed728708ba5b4d50a
LeeSSHH <0223samlee@gmail.com>
2019-09-19 20:36:37 +0800

    1

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,12 +1,14 @@
-$(document).on("click","#login",function () {
+$(document).on("click", "#login", function () {
     var username = $("#inputEmail").val();
     var password = $("#inputPassword").val();
     $.ajax({
-        url:"/login",
-        type:"POST",
-        dataType:"json",
-        data:{username:username, password:password},
-        success:function (result) {
+        url: "/login",
+        type: "POST",
+        dataType: "json",
+        async: true,
+        contentType: "application/json;charset=utf-8",
+        data: {"username": username, "password": password},
+        success: function (result) {
             if (result.code == 200) {
                 alert(result.msg);
             } else if (result.code == 401) {
